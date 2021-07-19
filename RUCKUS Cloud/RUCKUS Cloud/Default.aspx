9aa1ceda7a9b2b326b8cd844c9a5cad048c0c162
mmMOLINARI <68390677+mmMOLINARI@users.noreply.github.com>
2021-07-18 19:26:03 -0700

    Uploaded example to create Guest Pass

---
+++ RUCKUS Cloud/RUCKUS Cloud/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RUCKUS_Cloud.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            width: 100px;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <table style="width: 100%;">
+            <tr><td class="auto-style2"><asp:Label ID="Label1" runat="server" Text="Email:" Font-Names="Calibri" Font-Size="10pt"></asp:Label></td><td><asp:TextBox ID="txtEmail" runat="server" Width="300px" Font-Names="Calibri">marcelo.molinari@ruckuswireless.com</asp:TextBox></td></tr>
+        	<tr><td class="auto-style2"><asp:Label ID="Label2" runat="server" Text="Password:" Font-Names="Calibri" Font-Size="10pt"></asp:Label></td><td><asp:TextBox ID="txtPassword" runat="server" Width="300px" Font-Names="Calibri">password</asp:TextBox></td></tr>
+       		<tr><td class="auto-style2"><asp:Button ID="btnGetToken" runat="server" Text="Get Token" OnClick="getToken_Click" Font-Names="Calibri" /></td><td><asp:TextBox ID="txtToken" runat="server" Width="840px" Height="50px" TextMode="MultiLine" Font-Names="Calibri"></asp:TextBox></td></tr>
+        </table>
+       <table style="width: 100%;">
+            <tr><td class="auto-style2"><asp:Button ID="btnGetVenues" runat="server" Text="Get Venues" OnClick="getVenues_Click" Font-Names="Calibri" /></td><td><asp:TextBox ID="txtGetVenues" runat="server" Width="840px" Font-Names="Calibri" Height="50px" TextMode="MultiLine"></asp:TextBox></td></tr>
+            <tr><td class="auto-style2"><asp:Button ID="btvGetNetworks" runat="server" Text="Get Networks" OnClick="getNetworks_Click" Font-Names="Calibri" /></td><td><asp:TextBox ID="txtGetNetworks" runat="server" Width="840px" Font-Names="Calibri" Height="95px" TextMode="MultiLine"></asp:TextBox></td></tr>
+        	<tr><td class="auto-style2"><asp:Label ID="Label3" runat="server" Text="Name:" Font-Names="Calibri" Font-Size="10pt"></asp:Label></td><td><asp:TextBox ID="txtName" runat="server" Width="350px" Font-Names="Calibri">marcelo</asp:TextBox></td></tr>
+        	<tr><td class="auto-style2"><asp:Label ID="Label4" runat="server" Text="Phone:" Font-Names="Calibri" Font-Size="10pt"></asp:Label></td><td><asp:TextBox ID="txtPhone" runat="server" Width="350px" Font-Names="Calibri">+19541234567</asp:TextBox></td></tr>
+        	<tr><td class="auto-style2"><asp:Label ID="Label5" runat="server" Text="Network ID:" Font-Names="Calibri" Font-Size="10pt"></asp:Label></td><td><asp:TextBox ID="txtNetworkId" runat="server" Width="350px" Font-Names="Calibri">794e80f2314440fea8aa0f98da18ffff</asp:TextBox></td></tr>
+            <tr><td class="auto-style2"><asp:Button ID="btnCreateGuestPass" runat="server" Text="Create Pass" OnClick="createPass_Click" Font-Names="Calibri" /></td><td><asp:TextBox ID="txtCreatePass" runat="server" Width="840px" Font-Names="Calibri" Height="385px" TextMode="MultiLine"></asp:TextBox></td></tr>
+       	</table>
+    </form>
+</body>
+</html>
---
+++ RUCKUS Cloud/RUCKUS Cloud/Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace RUCKUS_Cloud
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public RestClient client = new RestClient("http://ruckus.cloud");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        protected void getToken_Click(object sender, EventArgs e)
+        {
+            client.CookieContainer = new System.Net.CookieContainer();
+            client.UseJson();
+            var request = new RestRequest("token", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new
+            {
+                username = txtEmail.Text,
+                password = txtPassword.Text
+            });
+            IRestResponse response = client.Execute(request);
+            dynamic api = Newtonsoft.Json.Linq.JObject.Parse(response.Content);
+            Session["API-KEY"] = api["API-KEY"].Value;
+            Session["tenantId"] = api["tenantId"].Value;
+            txtToken.Text = "token: " + api["API-KEY"].Value + System.Environment.NewLine + "tenantId: " + api["tenantId"].Value;
+        }
+
+        protected void getVenues_Click(object sender, EventArgs e)
+        {
+            var request = new RestRequest("api/tenant/" + Session["tenantId"] + "/venue", Method.GET);
+            request.AddCookie("API-KEY", Session["API-KEY"].ToString());
+            IRestResponse response = client.Execute(request);
+            txtGetVenues.Text = response.Content;
+        }
+
+        protected void getNetworks_Click(object sender, EventArgs e)
+        {
+            var request = new RestRequest("api/tenant/" + Session["tenantId"] + "/wifi/network", Method.GET);
+            request.AddCookie("API-KEY", Session["API-KEY"].ToString());
+            IRestResponse response = client.Execute(request);
+            txtGetNetworks.Text = response.Content;
+        }
+
+        protected void createPass_Click(object sender, EventArgs e)
+        {
+            var request = new RestRequest("api/tenant/" + Session["tenantId"] + "/wifi/guest-user", Method.POST);
+            request.AddCookie("API-KEY", Session["API-KEY"].ToString());
+            request.RequestFormat = DataFormat.Json;
+
+            Body root = new Body()
+            {
+                deliveryMethods = new List<string>() { "PRINT" },
+                expiration = new Expiration()
+                {
+                    activationType = "Creation",
+                    duration = 60,
+                    unit = "Minute",
+                },
+                maxDevices = 3,
+                mobilePhoneNumber = txtPhone.Text,
+                name = txtName.Text,
+                networkId = txtNetworkId.Text,
+            };
+
+            List<Body> wrapperList = new List<Body>();
+            wrapperList.Add(root);
+            //string json = JsonConvert.SerializeObject(wrapperList);
+            request.AddJsonBody(wrapperList);
+            IRestResponse response = client.Execute(request);
+            txtCreatePass.Text = response.Content;
+        }
+
+        public class Body
+        {
+            public List<string> deliveryMethods { get; set; }
+            public Expiration expiration { get; set; }
+            public int maxDevices { get; set; }
+            public string mobilePhoneNumber { get; set; }
+            public string name { get; set; }
+            public string networkId { get; set; }
+        }
+
+        public class Expiration
+        {
+            public string activationType { get; set; }
+            public int duration { get; set; }
+            public string unit { get; set; }
+        }
+    }
+}
